12aebe94a950c5a6e71baed7b1a62b7ddb15659f
Chris Fung <aergonaut@gmail.com>
2016-08-24 23:13:43 -0700

    more spec

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,5 +1,9 @@
 class Identity < ApplicationRecord
+  PROVIDERS = ["github"].freeze
+
   belongs_to :user
+
+  validates :provider, presence: true, inclusion: { in: Identity::PROVIDERS }
 
   attr_encrypted :access_key
 end
--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Identity, type: :model do
   it { is_expected.to belong_to :user }
+
+  it { is_expected.to validate_presence_of :provider }
+  it { is_expected.to validate_inclusion_of(:provider).in_array(Identity::PROVIDERS) }
 end
